eaeba3bb267e8757e36d0cdd9683aadea19341a5
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2026-06-25 01:30:10 +0600

    mack server

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+  res.send('server is running');
+});
+
+
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
