1f58a2efdb5fbf63d3a8f16692b66799bdd13d3c
sr-shuvobd <mdshohanurrahomanshuvo@gmail.com>
2026-06-25 21:55:42 +0600

    sum update

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const port = process.env.PORT || 5000;
 require('dotenv').config();
 
 app.use(cors({
-    origin: ['http://localhost:3000'],
+    origin: process.env.BETTER_AUTH_URL,
     credentials: true
 }));
 app.use(express.json());
@@ -33,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
-        await client.connect();
+        // await client.connect();
 
         const database = client.db("cooksphere");
         const recipesCollection = database.collection("recipes");
@@ -405,7 +405,7 @@ async function run() {
 
 
         // Send a ping to confirm a successful connection
-        await client.db("admin").command({ ping: 1 });
+        // await client.db("admin").command({ ping: 1 });
         console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } catch (error) {
         console.error(error);
